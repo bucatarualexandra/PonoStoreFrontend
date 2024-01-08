331511bcd00c8a715706cc3a296d0470008828a6
bucat <bucatarualexandra15@gmail.com>
2024-01-08 21:52:40 +0200

    NewCollections

--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -1,6 +1,8 @@
 import React from "react"
 import Hero from "../Components/Hero/Hero"
 import Popular from "../Components/Popular/Popular"
+import Offers from "../Components/Offers/Offers"
+import NewCollections from "../Components/NewCollections/NewCollections"
 
 const Shop = () => {
 
@@ -9,6 +11,8 @@ const Shop = () => {
         <div>
             <Hero/>
             <Popular/>
+            <Offers/>
+            <NewCollections/>
         </div>
 
     )
